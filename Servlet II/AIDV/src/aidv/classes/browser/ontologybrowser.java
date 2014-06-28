ee3a1091082e13f518454c8865d48da357327808
Stefan-V <s.voshage@gmx.de>
2014-06-28 17:40:58 +0200

    gui veränderung

--- Servlet II/AIDV/src/aidv/classes/browser/ontologybrowser.java
+++ Servlet II/AIDV/src/aidv/classes/browser/ontologybrowser.java
@@ -1,15 +1,17 @@
 package aidv.classes.browser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import aidv.classes.Annotation;
 import aidv.classes.Ontology;
 
 public abstract class OntologyBrowser {
-	protected List<Ontology> ontologys;
+	protected List<Ontology> ontologys=new ArrayList<Ontology>();
+	
 	public abstract Annotation get(Annotation annotation) throws IOException;
-
+	
 	public List<Ontology> getOntologys(){
 		return ontologys;
 	}
